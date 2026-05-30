66fece79af15082bc99613a8027ed2ab7b4b07be
creeeasy <aeourmassi@gmail.com>
2026-05-30 03:11:19 +0100

    fix(db): update fields

--- sihati-backend/src/config/sequelize-cli.js
+++ sihati-backend/src/config/sequelize-cli.js
@@ -8,30 +8,26 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    port: Number(process.env.DB_PORT || 5432),
     dialect: "postgres",
   },
 
   staging: {
-    use_env_variable: "DATABASE_URL_EXTERNAL",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 
   production: {
-    use_env_variable: "DATABASE_URL_INTERNAL",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 5432),
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 };
 
